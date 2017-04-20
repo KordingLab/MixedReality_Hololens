706a21e0502c51d4ba57c36e88cb258d28c8ea03
gaiqingkong <gqingkong@gmail.com>
2017-04-20 17:09:59 -0500

    Add files via upload

---
+++ Lab_1/Scripts/Lab_1_StaticObject.cs
@@ -0,0 +1,17 @@
+﻿//---------------------------------------------------
+//set stimulus parameters for static object 
+//---------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// </summary>
+
+public class Lab_1_StaticObject : Lab_1_StimulusBase
+{
+    void Awake()
+    {
+        ObjType = ObjTypes.Obj_Static;
+    }
+}
